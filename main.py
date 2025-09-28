33dfdff525660105bed5facf82d0fe4ac142260f
nurdauletzhuzbay <nurdauletzuzbaj@gmail.com>
2025-09-28 15:05:32 +0500

    time sleep decreased

--- main.py
+++ main.py
@@ -583,13 +583,13 @@ class MQTTRobotController:
             if not self._send_arduino_command_and_wait("robot_vertical_830"): return
             if not self._send_arduino_command_and_wait("robot_gripper_rotate_left"): return
             if not self._send_arduino_command_and_wait("robot_gripper_slide_forward"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_gripper_close"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_gripper_slide_backward"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_gripper_rotate_center"): return
-            time.sleep(5)
+            time.sleep(1)
 
             if not self._send_arduino_command_and_wait("robot_vertical_0"): return
 
@@ -599,21 +599,21 @@ class MQTTRobotController:
             self.servo_controller.move_to_position(delivery_pos, True, 30.0)
 
             if not self._send_arduino_command_and_wait("robot_vertical_350"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_gripper_rotate_right"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_gripper_slide_forward"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_gripper_open"): return
-            time.sleep(5)
+            time.sleep(1)
 
             self._publish_box_delivered(box_id)
             # self.logger.info(f"[SEQ] 📦 Box {box_id} delivered")
 
             if not self._send_arduino_command_and_wait("robot_gripper_slide_backward"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_gripper_rotate_center"): return
-            time.sleep(5)
+            time.sleep(1)
             if not self._send_arduino_command_and_wait("robot_vertical_0"): return
 
             # self.logger.info(f"[SEQ] Returning to home position")
